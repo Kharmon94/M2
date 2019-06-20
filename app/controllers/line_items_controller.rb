24ca11c067b7eaad0e26a4f0a9f027c2e7e4ba08
smplyamzng <kharmon9452@gmail.com>
2019-06-20 11:31:04 -0400

    added order to line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,11 +5,11 @@ class LineItemsController < InheritedResources::Base
 
 
 
+
 	def create
 		product = Product.find(params[:product_id])
 		@line_item = @cart.add_product(product)
-		Order.new
-
+		
 		respond_to do |format|
 			if @line_item.save
 				format.html { redirect_to @line_item.cart, notice: 'Item added to cart.'}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_144051) do
+ActiveRecord::Schema.define(version: 2019_06_20_145228) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -79,7 +79,9 @@ ActiveRecord::Schema.define(version: 2019_06_18_144051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
+    t.integer "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
